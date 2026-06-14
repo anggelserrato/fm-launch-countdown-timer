03b215e622ec74ea4d8225f5dd8ff8280865f389
Angel Serrato <serrato.anggel@gmail.com>
2026-06-13 20:38:47 -0500

    refactor: body background and add slate colors tokens

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${redHatText.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col bg-slate-900">{children}</body>
     </html>
   );
 }
